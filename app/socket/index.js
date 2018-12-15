3435be40646d064c4ffbb4b87958217f6e0e20ab
Gray Smith <gray.smith00@gmail.com>
2018-12-15 15:28:45 -0700

    Demo chatrooms displaying on rooms page through socket.io

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,6 +1,21 @@
 module.exports = (io, app) => {
   const allRooms = app.locals.chatrooms;
+
+  allRooms.push({
+    room: 'Good Food',
+    roomID: '0001',
+    users: []
+  });
+
+  allRooms.push({
+    room: 'Cloud Computing',
+    roomID: '0002',
+    users: []
+  });
+
   io.of('/roomslist').on('connection', socket => {
-    console.log('socket.io connected to client');
+    socket.on('getChatRooms', () => {
+      socket.emit('chatRoomsList', JSON.stringify(allRooms));
+    });
   });
 };
